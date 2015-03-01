95cacc3e59b528ae1484f324db6a2e6388ca26b8
Paul Grever <paulgrever@gmail.com>
2015-03-01 16:33:52 -0700

    added validations for name in compnay

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) do 
+    Company.new(name: "Sears")
+  end 
+
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is not valid without a name" do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
 end
