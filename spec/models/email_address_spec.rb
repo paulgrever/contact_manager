f93eb09ee5ab2d1865db68c506b9965b2f0e5ca3
Paul Grever <paulgrever@gmail.com>
2015-03-01 13:27:20 -0700

    added validation to ensure email address has field person_id

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
   let (:email_address) do 
-    EmailAddress.new(address: "john@yahoo.com")
+    EmailAddress.new(address: "john@yahoo.com", person_id: 1)
   end
 
   it "is valid" do
@@ -11,6 +11,11 @@ RSpec.describe EmailAddress, type: :model do
 
   it "is invalid without an email address" do
     email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it "is invalid with a person id" do
+    email_address.person_id = nil
     expect(email_address).to_not be_valid
     
   end
