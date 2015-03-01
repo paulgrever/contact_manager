c9d679061775fe6d8dd9199bf78aa9a3d1f33d5c
Paul Grever <paulgrever@gmail.com>
2015-03-01 13:24:12 -0700

    Added relationships and was able to get all tests to pass

--- spec/controllers/email_addresses_controller_spec.rb
+++ spec/controllers/email_addresses_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe EmailAddressesController, type: :controller do
   # EmailAddress. As you add validations to EmailAddress, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {address: "test@yahoo.com", person_id: 1}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {address: nil, person_id: nil}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -102,15 +102,16 @@ RSpec.describe EmailAddressesController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+      let(:valid_attributes) { {address: 'bob@yahoo.com', person_id: bob.id} }
+      let(:new_attributes) { {address: 'bob2@gmail.com', person_id: bob.id} }
 
       it "updates the requested email_address" do
         email_address = EmailAddress.create! valid_attributes
         put :update, {:id => email_address.to_param, :email_address => new_attributes}, valid_session
         email_address.reload
-        skip("Add assertions for updated state")
+        expect(email_address.address).to eq('bob2@gmail.com')
+        expect(email_address.person_id).to eq(1)
       end
 
       it "assigns the requested email_address as @email_address" do
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:email_address) do 
+    EmailAddress.new(address: "john@yahoo.com")
+  end
+
+  it "is valid" do
+    expect(email_address).to be_valid
+  end
+
+  it "is invalid without an email address" do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+    
+  end
 end
